7681c2cf48a58a05f128ff80333f98bf48e0d988
Xenia Vislocka <xvislock@c2r2s4.42prague.com>
2024-08-04 11:48:12 +0200

    4.8.2024 Read file

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -93,6 +93,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
+// Custom functions
+
+char	*ft_strjoin_free(char *str1, char const *str2);
+
 /* GNL Prototypes */
 
 char	*ft_strjoin_gnl(char *str1, char *str2);
--- main.c
+++ main.c
@@ -9,74 +9,18 @@
 #include "fdf.h"
 // #define SGN(_x) ((_x) >= 0 || -1)
 
-int *ft_transform_2d(int p3[3], int p2[2])
+void print_arr(char **arr)
 {
-	p2[0] = p3[0] * cos(120) + p3[1] * cos(122) + p3[2] * cos(118);
-	p2[1] = p3[0] * sin(120) + p3[1] * sin(122) + p3[2] * sin(118);
-	return (p2);
+	int i;
+	i = 0;
+
+	while(arr[i])
+	{
+		printf("%s", arr[i]);
+		i++;
+	}
+	printf("\n");
 }
-void draw_cube(t_map *map)
-{
-		// DEFINE 8 POINTS IN 3D
-
-		int A[3] = {500, 200, 0};
-		int B[3] = {550, 200, 0};
-		int C[3] = {550, 250, 0};
-		int D[3] = {500, 250, 0};
-		int E[3] = {500, 200, 70};
-		int F[3] = {550, 200, 70};
-		int G[3] = {550, 250, 70};
-		int H[3] = {500, 250, 70};
-
-		// TRANSFORM THE POINTS TO 2D
-
-		int AA[2];
-		int BB[2];
-		int CC[2];
-		int DD[2];
-		int EE[2];
-		int FF[2];
-		int GG[2];
-		int HH[2];
-		int test[2] = {235, 395};
-
-		ft_transform_2d(A, AA);
-		ft_transform_2d(B, BB);
-		ft_transform_2d(C, CC);
-		ft_transform_2d(D, DD);
-		ft_transform_2d(E, EE);
-		ft_transform_2d(F, FF);
-		ft_transform_2d(G, GG);
-		ft_transform_2d(H, HH);
-
-		// DRAW THE POINTS TO THE MAP
-
-		ft_put_points_2d(map, test);
-		ft_put_points_2d(map, AA);
-		ft_put_points_2d(map, BB);
-		ft_put_points_2d(map, CC);
-		ft_put_points_2d(map, DD);
-		ft_put_points_2d(map, EE);
-		ft_put_points_2d(map, FF);
-		ft_put_points_2d(map, GG);
-		ft_put_points_2d(map, HH);
-
-		// CONNECT THE POINTS
-
-		ft_bresenham(map, AA, BB);
-		ft_bresenham(map, BB, CC);
-		ft_bresenham(map, CC, DD);
-		ft_bresenham(map, DD, AA);
-		ft_bresenham(map, EE, FF);
-		ft_bresenham(map, FF, GG);
-		ft_bresenham(map, GG, HH);
-		ft_bresenham(map, HH, EE);
-		ft_bresenham(map, EE, AA);
-		ft_bresenham(map, FF, BB);
-		ft_bresenham(map, GG, CC);
-		ft_bresenham(map, HH, DD);
-}
-
 int32_t main(int argc, char **argv)
 {
 	t_map map;
@@ -95,19 +39,30 @@ int32_t main(int argc, char **argv)
 	memset(img->pixels, 255, img->width * img->height * sizeof (int32_t));
 
 	// reading a file
-	char *res;
+	char *row;
+	char *mapdata;
 	int fd;
+	int row_len;
+	int row_count;
 	fd = open("./test_maps/42.fdf", O_RDONLY);
+	row = get_next_line(fd);
+	mapdata = ft_strdup("");
+	row_len = ft_strlen(row);
+	row_count = 0;
 
-	res = get_next_line(fd);
-	while(res != NULL)
+	while(row != NULL)
 	{
-		printf("%s", res);
-		free(res);
-		res = get_next_line(fd);
+		// printf("%s", row);
+		mapdata = ft_strjoin_free(mapdata, row);
+		free(row);
+		row = get_next_line(fd);
+		row_count++;
 	}
+
 	close(fd);
+	printf("\n%s\n\n", mapdata);
 	printf("%d\n", ft_strlen("hello"));
+	printf("%d\n", row_count);
 
 	// Register a hook and pass mlx as an optional param before calling mlx_loop!
 	// mlx_loop_hook(mlx, ft_hook, mlx);
