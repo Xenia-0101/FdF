cab69fcf475276501cb66a5367ed3162ae9c49e9
xenia-0101 <xeniavislocka@gmail.com>
2024-08-17 18:48:36 +0200

    17.8.2024 Fix ft_count_spaces

--- src/ft_fdf_utils.c
+++ src/ft_fdf_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fdf_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xvislock <xvislock@student.42.fr>          +#+  +:+       +#+        */
+/*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 19:53:06 by xvislock          #+#    #+#             */
-/*   Updated: 2024/08/17 15:00:17 by xvislock         ###   ########.fr       */
+/*   Updated: 2024/08/17 18:46:54 by xenia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,18 @@ void ft_reset_img(t_glib *glib)
 {
 	printf("resetting img...\n");
 	memset(glib->img->pixels, 0xFF, glib->img->width * glib->img->height * sizeof (int32_t));
+}
+
+int ft_count_spaces(char *s)
+{
+	int i;
+	char **srow;
+
+	srow = ft_split(s, ' ');
+	i = 0;
+	while (srow[i] && (srow[i][0] == '-' || ft_isdigit(srow[i][0])))
+	{
+		i++;
+	}
+	return (i);
 }
