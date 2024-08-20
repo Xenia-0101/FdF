81c63cb3ad7175e9533a41367fb98f41815dae7d
Xenia Vislocka <xvislock@c3r4s3.42prague.com>
2024-08-20 07:56:01 +0200

    20.8.2024 Norminette.

--- src/ft_bresenham.c
+++ src/ft_bresenham.c
@@ -6,13 +6,102 @@
 /*   By: xvislock <xvislock@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 19:30:12 by xvislock          #+#    #+#             */
-/*   Updated: 2024/08/14 17:55:01 by xvislock         ###   ########.fr       */
+/*   Updated: 2024/08/20 07:55:32 by xvislock         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void ft_init_line(t_line *l, int p1[2], int p2[2])
+static void	ft_bresenham_v(t_glib *glib, t_line *line);
+static void	ft_bresenham_h(t_glib *glib, t_line *line);
+static void	ft_init_line(t_line *l, int p1[2], int p2[2]);
+void		ft_bresenham(t_glib *glib, int start[2], int end[2]);
+
+/**
+ * @brief Draws a line connecting two points.
+ *
+ * Based on dx and dy values, selects the algorithm to draw a line
+ * between two points.
+ * - dx and dy determine if the line is more vertical or more horisontal.
+ *
+ * @param glib Data struct
+ * @param start Initial point
+ * @param end End point
+ */
+void	ft_bresenham(t_glib *glib, int start[2], int end[2])
+{
+	t_line	line;
+
+	ft_init_line(&line, start, end);
+	if (line.dx >= line.dy)
+	{
+		ft_bresenham_h(glib, &line);
+	}
+	else
+	{
+		ft_bresenham_v(glib, &line);
+	}
+}
+
+/**
+ * @brief Algorithm to draw a more horisontal line.
+ *
+ * * Uses Bresenham's algorithm to draw a line that is more vertical
+ * (passes through octants II, III, VI, VII).
+ *
+ * @param glib Data struct
+ * @param line Line struct
+ */
+static void	ft_bresenham_h(t_glib *glib, t_line *line)
+{
+	while (line->point[0] != line->end[0] + line->dx_sgn)
+	{
+		ft_put_point(glib, line->point);
+		line->err += line->slope;
+		if (line->err >= 0)
+		{
+			line->point[1] += line->dy_sgn;
+			line->err += line->errInc;
+		}
+		line->point[0] += line->dx_sgn;
+	}
+}
+
+/**
+ * @brief Algorithm to draw a more vertical line.
+ *
+ * Uses Bresenham's algorithm to draw a line that is more vertical
+ * (passes through octants II, III, VI, VII).
+ *
+ * @param glib Data struct
+ * @param line Line struct
+ */
+static void	ft_bresenham_v(t_glib *glib, t_line *line)
+{
+	while (line->point[1] != line->end[1] + line->dy_sgn)
+	{
+		ft_put_point(glib, line->point);
+		line->err += line->slope;
+		if (line->err >= 0)
+		{
+			line->point[0] += line->dx_sgn;
+			line->err += line->errInc;
+		}
+		line->point[1] += line->dy_sgn;
+	}
+}
+
+/**
+ * @brief Initialises Line struct values.
+ *
+ * Assigns values to initial, end and temp points of the line and calculates
+ * constants required for Bresenham's line algorithm.
+ *
+ * @param l Line struct pointer
+ * @param p1 Initial point
+ * @param p2 End point
+ */
+static void	ft_init_line(t_line *l, int p1[2], int p2[2])
 {
 	l->start[0] = p1[0];
 	l->start[1] = p1[1];
@@ -35,50 +124,5 @@ static void ft_init_line(t_line *l, int p1[2], int p2[2])
 		l->slope = 2 * l->dx;
 		l->err = -l->dy;
 		l->errInc = -2 * l->dy;
-	}
-}
-
-static void ft_bresenham_h(t_glib *glib, t_line *line)
-{
-	while (line->point[0] != line->end[0] + line->dx_sgn)
-	{
-		ft_put_point(glib, line->point);
-		line->err += line->slope;
-		if (line->err >= 0)
-		{
-			line->point[1] += line->dy_sgn;
-			line->err += line->errInc;
-		}
-		line->point[0] += line->dx_sgn;
-	}
-
-}
-
-static void ft_bresenham_v(t_glib *glib, t_line *line)
-{
-	while (line->point[1] != line->end[1] + line->dy_sgn)
-	{
-		ft_put_point(glib, line->point);
-		line->err += line->slope;
-		if (line->err >= 0)
-		{
-			line->point[0] += line->dx_sgn;
-			line->err += line->errInc;
-		}
-		line->point[1] += line->dy_sgn;
-	}
-}
-
-void ft_bresenham(t_glib *glib, int start[2], int end[2])
-{
-	t_line line;
-	ft_init_line(&line, start, end);
-	if (line.dx >= line.dy)
-	{
-		ft_bresenham_h(glib, &line);
-	}
-	else
-	{
-		ft_bresenham_v(glib, &line);
 	}
 }
